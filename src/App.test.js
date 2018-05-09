1be50ae6e9d59bd0ba67a7825ceae8c214097f06
Michael Myers <coldlife@gmail.com>
2018-05-09 01:11:24 -0400

    First Jest/Puppeteer tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const puppeteer = require('puppeteer')
+let browser
+let page
+beforeAll(async () => {
+  browser = await puppeteer.launch(isDebugging())
+  page = await browser.newPage()
+  await page.goto('http://localhost:3000/')
+  page.setViewport({width: 500, height: 2400 })
+})
+
+afterAll(() => {
+  if( isDebugging()) {
+    browser.close()
+  }
+})
+
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,
+    slowMo: 250,
+    devtools: true,
+  }
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
+}
+
+describe('on page load', () => {
+  test('h1 loads correctly', async() => {
+    
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400,
+      },
+      userAgent: ''
+    })
+
+    await page.goto('http://localhost:3000/')
+    const html = await page.$eval('.App-title', e => e.innerHTML)
+    expect(html).toBe('Welcome to React')
+  },
+  16000
+  )
+
+  test('nav loads correctly', async () => {
+    const navbar = await page.$eval('.navbar', el => el ? true : false)
+    const listItems = await page.$$('.nav-li')
+
+    expect(navbar).toBe(true)
+    expect(listItems.length).toBe(4)
+  })
+})
